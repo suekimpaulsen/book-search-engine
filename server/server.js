37d329389719ad4092de4fc12a2fb4e4e9a36d36
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-09-13 16:56:59 -0500

    server.js edited for apolloserver

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
 const path = require('path');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('/schemas');
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
