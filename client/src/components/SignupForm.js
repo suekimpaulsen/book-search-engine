93ac8333baf74c0478c3ffb4d9b101e817b5461d
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-09-13 19:53:58 -0500

    signup edited, build ready to be deployed

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -35,12 +35,6 @@ const SignupForm = () => {
         variables: { ...userFormData },
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
       Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
